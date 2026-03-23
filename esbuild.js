651b5b0f0e3ff828190bde33b09bf39e54f5b4d5
Kwisma <77215384+Kwisma@users.noreply.github.com>
2026-03-23 18:46:16 +0800

    faet:  更新版本

--- esbuild.js
+++ esbuild.js
@@ -1,8 +1,5 @@
-// build.js
-const esbuild = require('esbuild');
-
+import esbuild from 'esbuild';
 const artifacts = [
-    { src: 'src/vercel.js', dest: 'dist/vercel.js' },
     { src: 'src/_worker.js', dest: 'dist/_worker.js' },
 ];
 
